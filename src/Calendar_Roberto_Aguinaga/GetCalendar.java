1e5f74033ffba9571e4ec0d0e17ed6eb71b6c6e9
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-20 12:14:44 -0500

    Added a constructor to get a new instance of a calendar whenever a new GetCal object is constructed.

--- src/Calendar_Roberto_Aguinaga/GetCalendar.java
+++ src/Calendar_Roberto_Aguinaga/GetCalendar.java
@@ -1,7 +1,15 @@
 package Calendar_Roberto_Aguinaga;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class GetCalendar {
-    
+
+    Calendar instanceCalendar;
+
+    public GetCalendar() {
+        instanceCalendar = Calendar.getInstance();
+    }
+
+   
 }
