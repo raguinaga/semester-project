7aa1e4e4c9ddd1f77d9059620a13fc2642995c20
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-20 12:02:06 -0500

    Added javafx imports. Added main and start methods.

--- src/Calendar_Roberto_Aguinaga/Main.java
+++ src/Calendar_Roberto_Aguinaga/Main.java
@@ -1,4 +1,20 @@
 package Calendar_Roberto_Aguinaga;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
