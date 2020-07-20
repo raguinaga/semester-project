68f038b0163958840ac41f88a5c55678dc7beb45
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-20 13:24:28 -0500

    Renamed class to better reflect functionality. Set title to stage.

--- src/Calendar_Roberto_Aguinaga/CalendarGUI.java
+++ src/Calendar_Roberto_Aguinaga/CalendarGUI.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-
+        stage.setTitle("To Do Calendar");
     }
 
     public static void main(String[] args) {
