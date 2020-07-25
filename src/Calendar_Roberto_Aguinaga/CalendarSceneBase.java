7fb7a9fb77181d60a123419368e4c91a80f35850
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-25 18:29:53 -0500

    Created a field for an hbox with name of weekdays

---
+++ src/Calendar_Roberto_Aguinaga/CalendarSceneBase.java
@@ -0,0 +1,9 @@
+package Calendar_Roberto_Aguinaga;
+
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+
+public class CalendarSceneBase {
+    protected HBox dayOfWeek;
+    
+}
