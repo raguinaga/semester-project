a2a00b1bfb025275f3d8bf077b5f1906f5100eab
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-25 18:18:32 -0500

    Tried out anchor pane

--- src/Calendar_Roberto_Aguinaga/CalendarGUI.java
+++ src/Calendar_Roberto_Aguinaga/CalendarGUI.java
@@ -1,6 +1,8 @@
 package Calendar_Roberto_Aguinaga;
 
 import javafx.application.Application;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
@@ -11,8 +13,16 @@ public class CalendarGUI extends Application {
 
     @Override
     public void start(Stage stage) {
-
+        Button login = new Button("login");
+        Button cancel = new Button("cancel");
+        HBox hbox = new HBox(10, login, cancel);
+        AnchorPane root = new AnchorPane(hbox);
+        AnchorPane.setRightAnchor(hbox, 10d);
+        AnchorPane.setBottomAnchor(hbox, 10d);
+        Scene scene = new Scene(root);
         stage.setTitle("To Do Calendar");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
