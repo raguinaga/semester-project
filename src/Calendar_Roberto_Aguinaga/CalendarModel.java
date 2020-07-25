0f1cb81c94f0d972f625ff5ed46e7039edc568ba
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-25 15:02:39 -0500

    Created a very basic calendar model

--- src/Calendar_Roberto_Aguinaga/CalendarModel.java
+++ src/Calendar_Roberto_Aguinaga/CalendarModel.java
@@ -1,4 +1,25 @@
 package Calendar_Roberto_Aguinaga;
 
 public class CalendarModel {
+    private String name;
+    private int startYear;
+    private int endYear;
+
+    public CalendarModel(String name, int startYear, int endYear) {
+        this.name = name;
+        this.startYear = startYear;
+        this.endYear = endYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStartYear() {
+        return startYear;
+    }
+
+    public int getEndYear() {
+        return endYear;
+    }
 }
