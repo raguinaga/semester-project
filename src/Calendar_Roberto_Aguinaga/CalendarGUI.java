981ce31b29f20070cd1d17539f36982568728b59
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-25 20:36:23 -0500

    Organized imports and added more javafx layouts

--- src/Calendar_Roberto_Aguinaga/CalendarGUI.java
+++ src/Calendar_Roberto_Aguinaga/CalendarGUI.java
@@ -1,10 +1,14 @@
 package Calendar_Roberto_Aguinaga;
 
 import javafx.application.Application;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.DatePicker;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
@@ -13,7 +17,9 @@ public class CalendarGUI extends Application {
 
     @Override
     public void start(Stage stage) {
-
+        BorderPane root;
+        GridPane calendarGrid;
+        Scene scene = new Scene(root);
         stage.setTitle("To Do Calendar");
         //stage.setScene();
         stage.show();
