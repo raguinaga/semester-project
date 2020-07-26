1784d2cb130a066aa5b614e15ea8d38f69ee6ff6
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-26 13:09:32 -0500

    Finished label loop

--- src/Calendar_Roberto_Aguinaga/CreateCalendarScene.java
+++ src/Calendar_Roberto_Aguinaga/CreateCalendarScene.java
@@ -1,23 +1,28 @@
 package Calendar_Roberto_Aguinaga;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 
 public class CreateCalendarScene {
     private static final int WEEKDAYS = 7; // days in a week
-    private static final int WEEKROWS = 6; // I honestly just based based
+    private static final int WEEKROWS = 6; // I honestly just based
     // this off windows' calendar in the taskbar.
 
     private static void createWeekHeader() {
-      HBox headerBox = new HBox();
-      headerBox.setPrefSize(600, 30); // Trying out sizes
-      String[] days = {"Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"};
+        HBox headerBox = new HBox(); // top level hbox
+        headerBox.setPrefSize(600, 30); // Trying out sizes
+        String[] days = {"Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"};
 
-      for (int i = 0; i < days.length; i++) {
-        HBox lblBox = new HBox(new Label(days[i]));
-        HBox.setHgrow(lblBox, Priority.ALWAYS);
-      }
+        for (int i = 0; i < days.length; i++) {
+            HBox lblBox = new HBox(new Label(days[i]));
+            HBox.setHgrow(lblBox, Priority.ALWAYS);
+            lblBox.setMaxWidth(Double.MAX_VALUE);
+            lblBox.setMinWidth(headerBox.getPrefWidth()/7);
+            headerBox.getChildren().add(lblBox);
+        }
     }
-  
+
 }
