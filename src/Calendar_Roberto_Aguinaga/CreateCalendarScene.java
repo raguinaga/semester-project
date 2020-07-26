2fc7aee78b4357026ff7b7b764059168930c5635
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-26 13:22:08 -0500

    Removed unused Pos import, added comments to for loop.

--- src/Calendar_Roberto_Aguinaga/CreateCalendarScene.java
+++ src/Calendar_Roberto_Aguinaga/CreateCalendarScene.java
@@ -1,6 +1,5 @@
 package Calendar_Roberto_Aguinaga;
 
-import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -16,13 +15,24 @@ public class CreateCalendarScene {
         headerBox.setPrefSize(600, 30); // Trying out sizes
         String[] days = {"Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"};
 
+        // This sets up the day labels in the
         for (int i = 0; i < days.length; i++) {
+            // Create HBoxes to put into header HBox. These HBoxes
+            // will contain the labels that have the day names.
             HBox lblBox = new HBox(new Label(days[i]));
+
+            // Make sure that the Hboxes fill out the header HBox.
             HBox.setHgrow(lblBox, Priority.ALWAYS);
             lblBox.setMaxWidth(Double.MAX_VALUE);
             lblBox.setMinWidth(headerBox.getPrefWidth()/7);
+
+            // Add label HBoxes to header HBox
             headerBox.getChildren().add(lblBox);
         }
+    }
+
+    public static void createCalendar(GridPane gp) {
+        
     }
 
 }
