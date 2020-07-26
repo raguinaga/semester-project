57f1ed7dd1a169d8989911cdab408cfc4af96c9d
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-26 14:34:22 -0500

    Renamed method from addEvent to addNote

--- src/Calendar_Roberto_Aguinaga/noteHandler.java
+++ src/Calendar_Roberto_Aguinaga/noteHandler.java
@@ -2,14 +2,15 @@ package Calendar_Roberto_Aguinaga;
 
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import java.io.*;
 
 public class noteHandler {
-    
+
     // Set up a scene where user can add a note for the day
 
     Scene noteScene;
 
-    public static void addEvent(VBox day) {
+    public static void addNote(VBox day) {
 
     }
 }
