57ca8532dd4a6a2fcf63e016b9df016925194724
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-25 23:42:00 -0500

    Got rid of the gridpane method, started populating the borderpane

--- src/Calendar_Roberto_Aguinaga/CalendarGUI.java
+++ src/Calendar_Roberto_Aguinaga/CalendarGUI.java
@@ -21,23 +21,40 @@ public class CalendarGUI extends Application {
 
     @Override
     public void start(Stage stage) {
+        // Get date at program launch, so the default calendar scene
+        // is the current year/month
         LocalDate defaultDate = LocalDate.now();
         CalendarModel defaultModel =
                 new CalendarModel(defaultDate.getMonthValue(),
                         defaultDate.getYear());
 
-        BorderPane root;
+        // Create the top-level container, a border pane
+        BorderPane root = new BorderPane();
+
+        // Create a date picker and an exit button for the top pane,
+        // add them both.
+        DatePicker datePicker = new DatePicker();
+        Button exitButton = new Button("Exit");
+        HBox topBox = new HBox(datePicker, exitButton);
+        root.setTop(topBox);
+
         Scene scene = new Scene(root);
         stage.setTitle("To Do Calendar");
-        //stage.setScene();
+        stage.setScene(scene);
         stage.show();
     }
 
-    public GridPane initializeCalendarGrid(CalendarModel cm) {
+    public void initializeCalendarGrid(CalendarModel cm) {
         GridPane calendarGrid;
         int columns = 7; // days in a week
-        int rows = 6; // 
+        int rows = 6; // I honestly just based this off the # of rows
+                      // in the microsoft calendar preview-thing in the
+                      // taskbar
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
 
+            }
+        }
     }
 
     public static void main(String[] args) {
