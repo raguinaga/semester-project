428d2ac5f12c3915094d6b61ee6c02816189355a
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-26 11:35:06 -0500

    Started making a new method for creating the day headers

--- src/Calendar_Roberto_Aguinaga/CreateCalendarScene.java
+++ src/Calendar_Roberto_Aguinaga/CreateCalendarScene.java
@@ -6,6 +6,11 @@ import javafx.scene.layout.*;
 
 public class CreateCalendarScene {
     private static final int WEEKDAYS = 7; // days in a week
-    private static final int WEEKROWS = 6; // I honestly just based
+    private static final int WEEKROWS = 6; // I honestly just based based
+    // this off windows' calendar in the taskbar.
+
+    private static void createWeekHeader() {
+      Hbox header = new HBox();
+    }
   
 }
