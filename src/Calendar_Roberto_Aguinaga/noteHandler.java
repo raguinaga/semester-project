441b1f04333ca7d90d5e546a4ab20656a7c4b30c
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-26 14:30:28 -0500

    Formatted and added some imports.

--- src/Calendar_Roberto_Aguinaga/noteHandler.java
+++ src/Calendar_Roberto_Aguinaga/noteHandler.java
@@ -1,8 +1,14 @@
 package Calendar_Roberto_Aguinaga;
 
+import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 
 public class noteHandler {
+    
+    // Set up a scene where user can add a note for the day
+
+    Scene noteScene;
+
     public static void addEvent(VBox day) {
 
     }
