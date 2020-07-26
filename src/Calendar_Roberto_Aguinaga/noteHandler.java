7a7b59d2a4345ba5998237bb4d95549de592c160
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-26 14:28:11 -0500

    Started adding a method to add a note to a day VBox cell

--- src/Calendar_Roberto_Aguinaga/noteHandler.java
+++ src/Calendar_Roberto_Aguinaga/noteHandler.java
@@ -1,10 +1,9 @@
 package Calendar_Roberto_Aguinaga;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import javafx.scene.layout.VBox;
 
-public class getMonth {
+public class noteHandler {
+    public static void addEvent(VBox day) {
 
+    }
 }
