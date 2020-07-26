a21327be3f876e3288c880d047adbc8d0569f130
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-26 00:20:56 -0500

    Using vbox instead of an anchor pane for the container

--- src/Calendar_Roberto_Aguinaga/CalendarGUI.java
+++ src/Calendar_Roberto_Aguinaga/CalendarGUI.java
@@ -2,18 +2,17 @@ package Calendar_Roberto_Aguinaga;
 
 import javafx.application.Application;
 import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.GridPane;
 import javafx.scene.control.DatePicker;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.geometry.Pos;
 
+import java.security.PolicySpi;
 import java.time.LocalDate;
 import java.time.Month;
 
@@ -21,6 +20,7 @@ public class CalendarGUI extends Application {
 
     @Override
     public void start(Stage stage) {
+
         // Get date at program launch, so the default calendar scene
         // is the current year/month
         LocalDate defaultDate = LocalDate.now();
@@ -28,15 +28,12 @@ public class CalendarGUI extends Application {
                 new CalendarModel(defaultDate.getMonthValue(),
                         defaultDate.getYear());
 
-        // Create the top-level container, a border pane
-        BorderPane root = new BorderPane();
+        // Create the top-level container, a VBox.
+        VBox root = new VBox();
+        Label datePickerLabel  = new Label("Use the form to pick a " +
+                "new date");
+        HBox datePickBox = new HBox(new DatePicker());
 
-        // Create a date picker and an exit button for the top pane,
-        // add them both.
-        DatePicker datePicker = new DatePicker();
-        Button exitButton = new Button("Exit");
-        HBox topBox = new HBox(datePicker, exitButton);
-        root.setTop(topBox);
 
         Scene scene = new Scene(root);
         stage.setTitle("To Do Calendar");
