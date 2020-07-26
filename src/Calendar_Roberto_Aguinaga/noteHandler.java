64eb1074a5ada8f47a4e4ba68eaa3f8bb8b25df6
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-26 14:50:26 -0500

    Added new container for scene, new method for displaying area

--- src/Calendar_Roberto_Aguinaga/noteHandler.java
+++ src/Calendar_Roberto_Aguinaga/noteHandler.java
@@ -2,15 +2,24 @@ package Calendar_Roberto_Aguinaga;
 
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.ListView;
+
 import java.io.*;
 
 public class noteHandler {
 
     // Set up a scene where user can add a note for the day
-
+    SplitPane root = new SplitPane();
     Scene noteScene;
 
     public static void addNote(VBox day) {
+        
+        TextArea writeNote = new TextArea();
+    }
+
+    public static void displayNote() {
 
     }
 }
