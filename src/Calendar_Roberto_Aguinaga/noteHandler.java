85c8c69e131e0990f4c28783837b7661a9222cde
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-26 15:25:45 -0500

    Added a Vbox to the anchor pane

--- src/Calendar_Roberto_Aguinaga/noteHandler.java
+++ src/Calendar_Roberto_Aguinaga/noteHandler.java
@@ -1,10 +1,13 @@
 package Calendar_Roberto_Aguinaga;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.ListView;
+import javafx.scene.control.;
 
 import java.io.*;
 
@@ -12,14 +15,39 @@ public class noteHandler {
 
     // Set up a scene where user can add a note for the day
     SplitPane root = new SplitPane();
-    Scene noteScene;
+    Scene noteScene = new Scene(root);
 
-    public static void addNote(VBox day) {
-        
+    public void addNote(VBox day) {
+        Label aboutLbl = new Label("Type in the notes for today.");
+        // Create a textBox so user can input one note at a time.
         TextArea writeNote = new TextArea();
+        writeNote.setWrapText(true);
+
+        // Make an anchor pane with a button to save the note.
+        AnchorPane anchorPane = new AnchorPane();
+        Button saveNoteButton = new Button("Save note");
+        AnchorPane.setBottomAnchor(saveNoteButton,0.0);
+
+        VBox textAreaBox = new VBox(aboutLbl, writeNote, anchorPane);
+        textAreaBox.setPrefSize(300, 500);
+        root.getItems().add(textAreaBox);
     }
 
     public static void displayNote() {
+        Label aboutLbl = new Label("Check the items you wish to mark " +
+                "as done");
+        ListView<String> noteList = new ListView<>();
+        VBox listContainer = new VBox(noteList);
+
 
     }
+
+    protected String[] getNotes() {
+
+    }
+
+    public Scene getNoteScene() {
+        return noteScene;
+    }
+
 }
