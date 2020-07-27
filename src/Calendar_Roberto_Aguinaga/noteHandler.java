4606d175731e18c225a428aa7d51ae8ca0dc6162
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-27 14:19:10 -0500

    Removed more javafx imports, finished all read/write methods.

--- src/Calendar_Roberto_Aguinaga/noteHandler.java
+++ src/Calendar_Roberto_Aguinaga/noteHandler.java
@@ -1,14 +1,17 @@
 package Calendar_Roberto_Aguinaga;
 
-import javafx.stage.Stage;
-
 import java.io.*;
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class noteHandler {
 
+    private CalendarModel model;
+
+
     public boolean checkIfNoteExists(CalendarModel model) {
-        File file = new File(model.date);
+        this.model = model;
+        File file = new File(model.date + ".txt");
         if (file.exists()) {
             return true;
         } else {
@@ -22,19 +25,33 @@ public class noteHandler {
         File file = new File(filename);
         if (!flag) {
             try (
-                 FileWriter fileWriter = new FileWriter(file, false);
-                 PrintWriter printWriter = new PrintWriter(fileWriter)){
+                    FileWriter fileWriter = new FileWriter(file, false);
+                    PrintWriter printWriter = new PrintWriter(fileWriter)) {
                 printWriter.println(notes);
             } catch (IOException ioException) {
                 System.err.println(ioException.getMessage());
-            } // End try-catch clause
+            } // End of try-catch clause
         } else {
             try (FileWriter fWriter = new FileWriter(filename, true);
                  PrintWriter pWriter = new PrintWriter(fWriter)) {
-
+                pWriter.println(notes);
             } catch (IOException e) {
                 System.err.println(e.getMessage());
             }
         } // end of if-else statement
+    }
+
+    public ArrayList<String> readNotes() {
+        ArrayList<String> notes = new ArrayList<>();
+        File file = new File(model.date + ".txt");
+
+        try (Scanner noteScanner = new Scanner(file)) {
+            while (noteScanner.hasNextLine()) {
+                notes.add(noteScanner.nextLine());
+            }
+        } catch (FileNotFoundException foundException) {
+            System.err.println(foundException.getMessage());
+        }
+        return notes;
     }
 }
