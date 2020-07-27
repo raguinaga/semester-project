42fc6d8bc38f19b1ea6beb96971da963d7f0dd22
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-27 11:49:51 -0500

    Added a new class, added javafx imports, and added controls + layouts.

---
+++ src/Calendar_Roberto_Aguinaga/noteScene.java
@@ -0,0 +1,16 @@
+package Calendar_Roberto_Aguinaga;
+
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class noteScene {
+
+    private Scene noteScene;
+    private SplitPane splitPane;
+    private Button returnButton;
+    private Button saveNote;
+    private TextArea writeArea;
+    
+}
