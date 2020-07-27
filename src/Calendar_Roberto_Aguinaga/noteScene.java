1b8296cc4c1d71d79d1322e16baa049082f805e0
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-27 12:19:24 -0500

    Added a method to set up a splitpane, with a text area in a VBOX

--- src/Calendar_Roberto_Aguinaga/noteScene.java
+++ src/Calendar_Roberto_Aguinaga/noteScene.java
@@ -1,16 +1,47 @@
 package Calendar_Roberto_Aguinaga;
 
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class noteScene {
 
     private Scene noteScene;
+    private HBox buttonBox;
+    private VBox writeBox = new VBox();
+    private VBox displayBox = new VBox();
     private SplitPane splitPane;
     private Button returnButton;
     private Button saveNote;
     private TextArea writeArea;
-    
+
+    private void setUpWriteBox() {
+        writeArea = new TextArea();
+        writeArea.setWrapText(true);
+
+        returnButton = new Button("Return to calendar view");
+        saveNote = new Button("Save note");
+
+        buttonBox = new HBox(returnButton,saveNote);
+        buttonBox.setSpacing(15);
+
+        // Add to VBox
+        writeBox.getChildren().addAll(writeArea,buttonBox);
+    }
+
+    private void setUpDisplayBox() {
+        
+    }
+
+    public Scene getNoteScene() {
+        splitPane = new SplitPane();
+        splitPane.getItems().add(writeBox);
+        splitPane.setOrientation(Orientation.VERTICAL);
+
+        return noteScene;
+    }
 }
