d674e85874b9cc8e2771f29de4f23cdec054fd66
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-27 14:03:38 -0500

    Removed all methods related to javafx

--- src/Calendar_Roberto_Aguinaga/noteHandler.java
+++ src/Calendar_Roberto_Aguinaga/noteHandler.java
@@ -1,63 +1,43 @@
 package Calendar_Roberto_Aguinaga;
 
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.control.SplitPane;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
 import java.io.*;
+import java.time.LocalDate;
 
 public class noteHandler {
 
-    // Set up a scene where user can add a note for the day
-    SplitPane root = new SplitPane();
-    Scene noteScene = new Scene(root);
-
-    public void addNote(VBox day) {
-        Label aboutLbl = new Label("Type in the notes for today.");
-        // Create a textBox so user can input one note at a time.
-        TextArea writeNote = new TextArea();
-        writeNote.setWrapText(true);
-
-        // Make an anchor pane with a button to save the note.
-        AnchorPane anchorPane = new AnchorPane();
-        Button saveNoteButton = new Button("Save note");
-        AnchorPane.setBottomAnchor(saveNoteButton,0.0);
-
-        VBox textAreaBox = new VBox(aboutLbl, writeNote, anchorPane);
-        textAreaBox.setPrefSize(300, 500);
-        root.getItems().add(textAreaBox);
-    }
-
-    public static void displayNote() {
-        Label aboutLbl = new Label("Check the items you wish to mark " +
-                "as done");
-        ListView<String> noteList = new ListView<>();
-        VBox listContainer = new VBox(noteList);
-
-
-    }
-
-    /*protected String[] getNotes() {
-
-    }*/
-
-    /*protected void writeNotes(String note) {
-        File noteFile = new File(CalendarModel.monthName + );
-        try {
-
-        } catch (IOException e) {
-
+    public boolean checkIfNoteExists(CalendarModel model) {
+        File file = new File(model.date);
+        if (file.exists()) {
+            return true;
+        } else {
+            return false;
         }
-    }*/
-
-    public Scene getNoteScene() {
-        return noteScene;
     }
 
+    public void writeNote(CalendarModel cm, String notes) {
+        boolean flag = checkIfNoteExists(cm);
+        if (!flag) {
+            String filename = cm.date + ".txt";
+            File file = new File(filename);
+
+
+            try {
+                file.createNewFile();
+                FileWriter fileWriter = new FileWriter(file, false);
+                PrintWriter printWriter = new PrintWriter(fileWriter);
+                printWriter.println(notes);
+                printWriter.close();
+                fileWriter.close();
+            } catch (IOException ioException) {
+                System.err.println(ioException.getMessage());
+            } // End try-catch clause
+        } else {
+            try {
+               FileWriter fWriter = new FileWriter(file, true);
+            }
+        }
+
+    }
 }
