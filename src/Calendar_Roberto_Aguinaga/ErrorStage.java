9e36d170b19055d39909047c2984c1e4d7b098c3
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-28 22:16:19 -0500

    Created an error scene, did not work out as I thought

--- src/Calendar_Roberto_Aguinaga/ErrorStage.java
+++ src/Calendar_Roberto_Aguinaga/ErrorStage.java
@@ -1,2 +1,18 @@
-package Calendar_Roberto_Aguinaga;public class ErrorStage {
+package Calendar_Roberto_Aguinaga;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+
+public class ErrorStage extends Application {
+    @Override
+    public void start(Stage errorStage) {
+        Label errorLbl = new Label("☉_☉\n"+"Sorry,the program ran " +
+                "into a problem and had to close.");
+        errorLbl.setWrapText(true);
+        Scene errorScn = new Scene(errorLbl);
+        errorStage.show();
+    }
 }
