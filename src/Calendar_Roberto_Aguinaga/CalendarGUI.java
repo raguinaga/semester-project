eaef151a5efdc2559230b92b9677a56bf6703f04
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-28 19:32:50 -0500

    Removed unnecessary imports

--- src/Calendar_Roberto_Aguinaga/CalendarGUI.java
+++ src/Calendar_Roberto_Aguinaga/CalendarGUI.java
@@ -1,24 +1,15 @@
 package Calendar_Roberto_Aguinaga;
 
 import javafx.application.Application;
-import javafx.scene.control.Label;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-
-import javafx.scene.control.DatePicker;
-
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.geometry.Pos;
 
 
 public class CalendarGUI extends Application {
 
     @Override
     public void start(Stage window) {
-        // Create a calendar model with default constructor
-        //CalendarModel cm = new CalendarModel();
+        
         // Get the starting scene
         Scene defaultScene =
                 new CalendarScene().getCalendarScene(window);
