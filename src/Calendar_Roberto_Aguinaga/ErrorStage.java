8ca7c4537a017dba9eda21c9f9bebac3abdab092
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-28 22:59:24 -0500

    Refactored error stage class

--- src/Calendar_Roberto_Aguinaga/ErrorStage.java
+++ src/Calendar_Roberto_Aguinaga/ErrorStage.java
@@ -1,13 +1,23 @@
 package Calendar_Roberto_Aguinaga;
 
-import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 
-public class ErrorStage {
-    private Stage errorStg;
-    
+public abstract class ErrorStage {
+    protected static Stage errorStg;
+
+    public static void showMessage() {
+        Label errorLbl = new Label("☉_☉\n"+"Sorry,the program ran " +
+                "into a problem and had to close.");
+        errorLbl.setAlignment(Pos.CENTER);
+        errorLbl.setStyle("-fx-font-weight: bolder;" +
+                "-fx-font-size: 20");
+        errorLbl.setWrapText(true);
+        Scene errorScn = new Scene(errorLbl);
+        errorStg.setScene(errorScn);
+        errorStg.showAndWait();
+    }
 }
