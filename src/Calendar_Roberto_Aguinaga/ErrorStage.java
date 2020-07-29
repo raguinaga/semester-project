cbb6b96d1f2cfacdc07612e4dfb419a412e06480
Roberto Aguinaga <raguinaga1@outlook.com>
2020-07-28 22:50:29 -0500

    Stopped extending application, starting over.

--- src/Calendar_Roberto_Aguinaga/ErrorStage.java
+++ src/Calendar_Roberto_Aguinaga/ErrorStage.java
@@ -1,18 +1,13 @@
 package Calendar_Roberto_Aguinaga;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 
-public class ErrorStage extends Application {
-    @Override
-    public void start(Stage errorStage) {
-        Label errorLbl = new Label("☉_☉\n"+"Sorry,the program ran " +
-                "into a problem and had to close.");
-        errorLbl.setWrapText(true);
-        Scene errorScn = new Scene(errorLbl);
-        errorStage.show();
-    }
+public class ErrorStage {
+    private Stage errorStg;
+    
 }
