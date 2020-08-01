99e712d164e9da529a1bf643100e197a678bf8a5
Roberto Aguinaga <raguinaga1@outlook.com>
2020-08-01 18:26:01 -0500

    Tons of comment improvements

--- src/Calendar_Roberto_Aguinaga/CalendarScene.java
+++ src/Calendar_Roberto_Aguinaga/CalendarScene.java
@@ -24,6 +24,7 @@ import javafx.scene.layout.Priority;
 
 
 public class CalendarScene implements ReturnContent {
+
     // root container plus the calendar model to use
     private final VBox ROOT = new VBox();
     private CalendarModel calendarModel; // Calendar model
@@ -32,7 +33,8 @@ public class CalendarScene implements ReturnContent {
     private HBox dateNavBox = new HBox();
     private GridPane calendarGrid = new GridPane();
 
-    // Date picker for moving to other, plus button to actually go
+    // Date picker for moving to other dates, plus button to actually
+    // go to the other dates.
     private DatePicker datePicker = new DatePicker();
     private Button goToDate = new Button("Go to date");
 
@@ -43,7 +45,7 @@ public class CalendarScene implements ReturnContent {
     /**
      * Default constructor sets up a new Calendar Model based on
      * LocalDate's now method. Then calls the rest of the methods in
-     * the class to set up the Calander controls
+     * the class to set up the Calendar controls
      */
     public CalendarScene() {
         calendarModel = new CalendarModel();
@@ -55,14 +57,15 @@ public class CalendarScene implements ReturnContent {
     }
 
     /**
-     * Second Constructor sets up a CalendarModel copied from a
+     * CalendarModel Constructor sets up a CalendarModel copied from a
      * supplied parameter. It then calls all the rest of the set up
      * methods to create a new root pane.
      *
-     * @param cm
+     * @param cm A CalendarModel.
      */
     public CalendarScene(CalendarModel cm) {
-        calendarModel = new CalendarModel(cm); // copy constructor call
+        // copy constructor call
+        calendarModel = new CalendarModel(cm);
 
         // Call to the rest of the methods.
         setUpDatePicker();
@@ -71,7 +74,6 @@ public class CalendarScene implements ReturnContent {
         setDayLabels();
         setUpRoot();
     }
-
 
     /**
      * This method generates the weekday header.
@@ -85,7 +87,7 @@ public class CalendarScene implements ReturnContent {
         headerBox.setAlignment(Pos.CENTER);
         headerBox.setMaxWidth(847); // try to match the gridpane width
 
-        // Set up internal Hboxes and labels.
+        // Set up internal HBoxes and labels.
         for (String day : days) {
             // Create HBoxes / labels, these HBoxes will contain the
             // labels that have the day names.
@@ -108,7 +110,7 @@ public class CalendarScene implements ReturnContent {
     }
 
     /**
-     * Prepares the gridpane that makes up the body of the calendar.
+     * Prepares the GridPane that makes up the body of the calendar.
      */
     private void createCalendar() {
         // Loop through the 6 rows, and add vboxes 7 times to each row
@@ -128,15 +130,18 @@ public class CalendarScene implements ReturnContent {
         } // end of for-loop
 
         // Have to also add row and column constraints for consistent
-        // VBox size, this adds an extra row that I couldn't figure
-        // out how to avoid.
-        for (int i = 0; i < days; i++) {
+        // VBox size, had to split into 2 loops to avoid extra row
+        // being generated.
+        for (int i = 0; i < weeks; i++) {
             RowConstraints rc = new RowConstraints(90);
             calendarGrid.getRowConstraints().add(rc);
-            ColumnConstraints cc =
-                    new ColumnConstraints(120);
+        }
+        // ColumnConstraint loop
+        for (int i = 0; i < days; i++) {
+            ColumnConstraints cc = new ColumnConstraints(120);
             calendarGrid.getColumnConstraints().add(cc);
         }
+
         // Set style class for the grid
         calendarGrid.getStyleClass().add("cal-grid");
 
@@ -145,19 +150,21 @@ public class CalendarScene implements ReturnContent {
         gridBox.setAlignment(Pos.BOTTOM_CENTER);
     }
 
-
+    /**
+     * Goes back through the GridPane to add labels and event handlers
+     */
     public void setDayLabels() {
-        // Go back through each node in the gridpane and add the
-        // numbers for the days
-        int gridCount = 1;
-        int lblCount = 1;
+        int gridCount = 1; // Starts counting the grid
+        int lblCount = 1; // Starts counting the labels
+
+        // Gets the day of week the month starts on
         int offset = calendarModel.getFirstDay();
 
         for (Node node : calendarGrid.getChildren()) {
             VBox dayCell = (VBox) node; // have to cast nodes
 
             // If the month does not start on Sunday, make it dark, do
-            // not add number label
+            // not add number label/event handler
             if (gridCount <= offset) {
                 gridCount++;
                 node.setStyle("-fx-background-color: #737373");
@@ -167,32 +174,52 @@ public class CalendarScene implements ReturnContent {
                 if (lblCount > calendarModel.getDaysInMonth()) {
                     node.setStyle("-fx-background-color: #737373");
                 } else {
+                    // Add number labels
                     Label numberLbl = new Label(Integer.toString(lblCount));
                     dayCell.getChildren().add(numberLbl);
 
                     // Add an event handler to each cell, pass the
-                    // current day in the form of the label to notescene
+                    // current day in the form of the label to
+                    // NoteScene class constructor
                     dayCell.addEventHandler(MouseEvent.MOUSE_CLICKED,
                             event -> {
+                                // Set up a new NoteScene for every
+                                // VBox, avoiding nullpointer exceptions
                                 NoteScene noteScene =
                                         new NoteScene(numberLbl, calendarModel);
+
+                                // pass root node to whatever our
+                                // scene is. No more confusing/messy
+                                // scene referencing
                                 dayCell.getScene()
                                         .setRoot(noteScene.getContent());
                             });
-                }
+                } // End of nested if/else
                 lblCount++;
-            } // end of else-if chain
+            } // end of outer if/else
         } // end of for-each loop
     }
 
+    /**
+     * Sets up the DatePicker event handler so that it no longer
+     * throws null pointer exceptions when trying to move to valid
+     * dates. This block can still throw those exceptions but now we
+     * ignore them (great design, I know /s).
+     */
     private void setUpDatePicker() {
-        goToDate = new Button("Go to date");
         goToDate.setOnMouseClicked(event -> {
             try {
                 calendarModel = new CalendarModel(datePicker.getValue());
                 CalendarScene calScene = new CalendarScene(calendarModel);
                 goToDate.getScene().setRoot(calScene.getContent());
             } catch (NullPointerException ignored) {
+                /* In my defence, I tried using an if statement to
+                 * check if the value was null, but it still threw a
+                 * NP exception. After all what is my else block
+                 * supposed to do? After a while I just chose to
+                 * ignore this. The program runs fine even after the
+                 * exception is thrown.
+                 */
             }
         });
 
@@ -205,15 +232,18 @@ public class CalendarScene implements ReturnContent {
 
     /**
      * Takes all the other layout containers and places them in the
-     * root VBox. Also sets up the datePicker
+     * root VBox. Also sets up the NameBox and its interior containers
+     * I chose to keep that set up in this method to avoid more
+     * method calls in the constructor.
      */
     private void setUpRoot() {
 
-        // Set up a label and HBox for displaying the Date above
-        // the calendar
-        // Label telling user what month / year they are in.
+        // Set up a label and HBox for displaying the Date above the
+        // calendar
         Label nameLbl = new Label(calendarModel.getMonthName());
         HBox nameBox = new HBox(nameLbl);
+
+        // Set nameBox properties, add style class
         nameBox.setAlignment(Pos.CENTER);
         nameBox.setMaxWidth(600);
         nameBox.setMinHeight(70);
@@ -228,12 +258,17 @@ public class CalendarScene implements ReturnContent {
 
         ROOT.setMaxSize(1000, 900);
 
-        // load the stylesheet
+        // load the CSS stylesheet
         ROOT.getStylesheets().add(this.getClass().getResource(
                 "./styleRules.css").toExternalForm());
 
     }
 
+    /**
+     * Returns the root node for this scene graph.
+     * @return A Parent node that contains all the children nodes for
+     * the calendars
+     */
     @Override
     public Parent getContent() {
         return ROOT;
